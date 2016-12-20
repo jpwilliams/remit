0cfd9fb529f94f4af9fa2b5f0f08256bc3e09779
Jack Williams <jack@tagstr.co>
2016-12-20 16:53:44 +0000

    Don't use this

--- examples/reqRes/endpoint.js
+++ examples/reqRes/endpoint.js
@@ -1,18 +1,5 @@
 const Remit = require('../../')
 const remit = Remit()
-// const memwatch = require('memwatch-next')
-
-// var hd = new memwatch.HeapDiff()
-
-// memwatch.on('leak', (info) => {
-//   console.log('LEAK', info)
-//   var diff = hd.end()
-//   console.log('diff', require('util').inspect(diff, {depth: null}))
-// })
-
-// memwatch.on('stats', (stats) => {
-//   console.log('Stats', stats)
-// })
 
 remit
   .endpoint('my.worker.queue')
