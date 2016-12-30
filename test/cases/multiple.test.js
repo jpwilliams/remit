3c83b468c9382cb8c2356735293cdd4a3f5423cf
Jack Williams <jack@tagstr.co>
2016-12-30 11:48:42 +0000

    Create our endpoint before we send a message

--- test/cases/multiple.test.js
+++ test/cases/multiple.test.js
@@ -1,4 +1,4 @@
-/* global describe, it, expect, sinon */
+/* global describe, it, expect, sinon, before */
 
 const Remit = require('../../')
 const remit1 = Remit()
@@ -6,18 +6,21 @@ const remit2 = Remit()
 const endpointDataCallback = sinon.spy()
 const requestDataCallback = sinon.spy()
 
-remit2
-  .endpoint('cases.multiple')
-  .data(endpointDataCallback)
-  .data((event, callback) => {
-    return callback('errrrr')
-  })
-
 const request = remit1
   .request('cases.multiple')
   .data(requestDataCallback)
 
 describe('Cases#multiple', function () {
+  before(function (done) {
+    remit2
+      .endpoint('cases.multiple')
+      .data(endpointDataCallback)
+      .data((event, callback) => {
+        return callback('errrrr')
+      })
+      .ready(done)
+  })
+
   it('should get a reply', function (done) {
     request.send({foo: 'bar'}).then((result) => {
       return done('Should not be successful')
