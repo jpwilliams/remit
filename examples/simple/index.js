24916e6052703362bc95b8574abf70a306725020
Jack Williams <jack@tagstr.co>
2016-12-30 17:21:40 +0000

    Reformat this “simple” example to add listeners too

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -5,20 +5,47 @@ const remit = Remit({
 })
 
 const foo = remit
-  .request('my.worker.queue')
+  .request('my.worker.endpoint')
   .data((err, result) => {
     console.log(err, result)
   })
 
-remit
-  .endpoint('my.worker.queue')
-  .data((event, callback) => {
-    event.data.baz = 'qux'
+Promise.all([
+  remit
+    .endpoint('my.worker.endpoint')
+    .data((event, callback) => {
+      event.data.baz = 'qux'
 
-    console.log(event)
+      console.log('Emitting...')
+      remit.emit('my.worker.listener.hit').send(event)
+      console.log('Emitted.')
 
-    return callback(null, event.data)
-  })
-  .ready(() => {
+      return callback(null, event.data)
+    })
+    .ready(),
+
+  remit
+    .listen('my.worker.listener.hit')
+    .data((event, callback) => {
+      console.log(`Detected that "${event.eventType}" was hit by "${event.resource}" at ${event.timestamp}`)
+
+      return callback()
+    })
+    .ready(),
+
+  remit
+    .listen('my.worker.listener.hit')
+    .data(() => {
+      console.log(`Proving we can listen to two things...`)
+    })
+    .data(() => {
+      console.log('...and have multiple listeners for each.')
+    })
+    .data((event, callback) => {
+      return callback()
+    }).ready()
+]).then(() => {
+  setInterval(() => {
     foo({foo: 'bar'})
-  })
+  }, 3000)
+})
