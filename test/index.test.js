4ecd048e0ebe36702f5848996d08a57d082b50c0
Jack Williams <jack@tagstr.co>
2017-01-05 13:22:14 +0000

    Enabling some more tests

--- test/index.test.js
+++ test/index.test.js
@@ -3,16 +3,6 @@
 const Remit = require('../')
 
 describe('Remit', function () {
-  // let amqpCon = null
-
-  // before(function (after) {
-  //   amqplib.connect('amqp://localhost').then((connection) => {
-  //     amqpCon = connection
-
-  //     return after()
-  //   })
-  // })
-
   describe('#object', function () {
     it('should export a Remit function', function () {
       expect(Remit).to.be.a('function')
@@ -25,30 +15,35 @@ describe('Remit', function () {
       expect(remit._emitter.emit).to.be.a('function')
     })
 
-    // it('should export Request and Response functions', function () {
-    //   const remit = Remit()
+    it('should export Request and Response functions', function () {
+      const remit = Remit()
 
-    //   expect(remit.request).to.be.a('function')
-    //   expect(remit.transientRequest).to.be.a('function')
-    //   expect(remit.emit).to.be.a('function')
-    //   expect(remit.delayedEmit).to.be.a('function')
-    //   expect(remit.respond).to.be.a('function')
-    //   expect(remit.listen).to.be.a('function')
+      expect(remit.request).to.be.a('function')
+      expect(remit.req).to.equal(remit.request)
 
-    //   expect(remit.req).to.be.a('function')
-    //   expect(remit.treq).to.be.a('function')
-    //   expect(remit.demit).to.be.a('function')
-    //   expect(remit.res).to.be.a('function')
-    //   expect(remit.endpoint).to.be.a('function')
-    //   expect(remit.on).to.be.a('function')
-    // })
+      expect(remit.emit).to.be.a('function')
 
-    // it('should successfully emit events', function (done) {
-    //   const remit = Remit()
+      expect(remit.delayedEmit).to.be.a('function')
+      expect(remit.demit).to.equal(remit.delayedEmit)
 
-    //   remit._emitter.on('testEvent', done)
-    //   remit._emitter.emit('testEvent')
-    // })
+      expect(remit.respond).to.be.a('function')
+      expect(remit.res).to.equal(remit.respond)
+      expect(remit.endpoint).to.equal(remit.respond)
+
+      expect(remit.respondQueue).to.be.a('function')
+      expect(remit.resq).to.equal(remit.respondQueue)
+      expect(remit.queue).to.equal(remit.respondQueue)
+
+      expect(remit.listen).to.be.a('function')
+      expect(remit.on).to.equal(remit.listen)
+    })
+
+    it('should successfully emit events', function (done) {
+      const remit = Remit()
+
+      remit._emitter.on('testEvent', done)
+      remit._emitter.emit('testEvent')
+    })
   })
 
   describe('#init', function () {
