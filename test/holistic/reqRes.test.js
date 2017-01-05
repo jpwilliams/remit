09f6e08361aeac5996bf9baafc06f3359f7c6275
Jack Williams <jack@tagstr.co>
2017-01-05 13:22:33 +0000

    Adding a listener to our holistic req/res

--- test/holistic/reqRes.test.js
+++ test/holistic/reqRes.test.js
@@ -3,8 +3,11 @@
 describe('Holistic request/response', function () {
   const callback = sinon.spy()
   const callback2 = sinon.spy()
+  const callback3 = sinon.spy()
+  const callback4 = sinon.spy()
   let endpoint
   let request
+  let listener
 
   describe('#endpoint', function () {
     it('should set up an endpoint', function (done) {
@@ -12,6 +15,7 @@ describe('Holistic request/response', function () {
         .endpoint('holistic.request.response')
         .ready(done)
 
+      expect(endpoint).to.be.an('object')
       expect(endpoint.ready).to.be.a('function')
       expect(endpoint.data).to.be.a('function')
     })
@@ -19,6 +23,8 @@ describe('Holistic request/response', function () {
     it('should add three data callbacks', function () {
       const handler = function (event, done) {
         event.data.bar = 'baz'
+
+        remit.emit('holistic.request.response.hit').send(event.data)
 
         return done(null, event.data)
       }
@@ -62,13 +68,51 @@ describe('Holistic request/response', function () {
 
     it('should request data from the endpoint', function (done) {
       request
-        .data(() => {
+        .send({foo: 'bar'})
+        .then(() => {
           expect(callback).to.have.been.called
           expect(callback2).to.have.been.called
 
           return done()
         })
+    })
+  })
+
+  describe('#listener', function () {
+    it('should set up a listener', function () {
+      listener = remit.listen('holistic.request.response.hit')
+
+      expect(listener).to.be.an('object')
+      expect(endpoint.ready).to.be.a('function')
+      expect(endpoint.data).to.be.a('function')
+    })
+
+    it('should add three data callbacks', function () {
+      const handler = function (event, done) {
+        return done(null, event.data)
+      }
+
+      listener
+        .data(callback3)
+        .data(callback4)
+        .data(handler)
+
+      expect(listener._emitter._events.data).to.be.an('array')
+      expect(listener._emitter._events.data).to.have.lengthOf(3)
+      expect(listener._emitter._events.data[0].fn).to.equal(callback3)
+      expect(listener._emitter._events.data[1].fn).to.equal(callback4)
+      expect(listener._emitter._events.data[2].fn).to.equal(handler)
+    })
+
+    it('should be hit as a result of calling the request', function (done) {
+      request
         .send({foo: 'bar'})
+        .then(() => {
+          expect(callback3).to.have.been.called
+          expect(callback4).to.have.been.called
+
+          return done()
+        })
     })
   })
 })
