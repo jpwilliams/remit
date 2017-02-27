4145ecf1878a032438823a2614ad0a149530000e
Jack Williams <jpwilliamsphotography@gmail.com>
2017-02-27 09:27:59 +0000

    #16 Define an available prefetch limit, defaulting to 128

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function Remit (opts) {
     this._url = opts.url || 'amqp://localhost'
     this._trace = opts.trace === false ? false : true
     this._exchange_name = opts.exchange || 'remit'
+    this._prefetch = opts.prefetch || 128
 
     // Global items
     this._connection = opts.connection || null
