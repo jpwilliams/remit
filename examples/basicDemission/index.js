a1777ddc9397ed5813db0711ecff38b07810d0bc
Jack Williams <jpwilliamsphotography@gmail.com>
2017-07-04 14:20:05 +0100

    :memo: Edit demission example to use new emit/delay

--- examples/basicDemission/index.js
+++ examples/basicDemission/index.js
@@ -2,14 +2,18 @@ const remit = require('../../')({
   name: 'examplesBasicDemission'
 })
 
+const demit = remit.emit('examples.basic.demission')
+
 remit.listen('examples.basic.demission')
   .data((event, callback) => {
-    console.log('Got event', event)
+    console.log('Got event', event.eventId, event.delay)
 
     return callback(null, true)
   })
   .ready().then(() => {
-    Array.from([5000, 7000, 1000, 3000, 4000, 9000, 2000, 10000, 6000, 8000]).forEach((time) => {
-      remit.demit('examples.basic.demission')({foo: 'bar'}, (new Date(+new Date() + time)))
+    Array.from([5000, 7000, 1000, 3000, 4000, 5000, 2000, 2000, 2000, 9000, 2000, 10000, 6000, 8000, 30000, 30000, 30000]).forEach((time) => {
+      demit({foo: 'bar'}, {
+        delay: time
+      })
     })
   })
