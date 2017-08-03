cb51ac35a9a08f1d75bb96468543d31b91820e2c
Jack Williams <jpwilliamsphotography@gmail.com>
2017-08-03 10:20:03 +0100

    📋 Example tweaks for #37 testing

--- examples/requestPromises/index.js
+++ examples/requestPromises/index.js
@@ -6,14 +6,23 @@ remit.request.data(() => {
   console.log('Got data')
 })
 
+remit.request.sent(() => {
+  console.log('Sent request')
+})
+
+console.log(remit.request)
+
 const myRequest = remit
-  .request('my.worker.queue')
+  .request('my.worker.queue.2')
   .data((err, result) => {
-    console.log('CALLBACK', err, result)
+    console.log('request.data #1', err, result)
+  })
+  .sent(function () {
+    console.log('request.sent #1')
   })
 
 remit
-  .endpoint('my.worker.queue')
+  .endpoint('my.worker.queue.2')
   .data((event, callback) => {
     event.data.baz = (event.data.baz || '') + 'qux'
 
@@ -21,22 +30,22 @@ remit
   })
   .ready().then(() => {
     myRequest.data((err, result) => {
-      console.log('CALLBACK SECOND', err, result)
+      console.log('request.data #2', err, result)
     })
 
     return myRequest({foo: 'bar'})
   }).then((result) => {
-    console.log('PROMISE', result)
+    console.log('request promise #1', result)
 
     return myRequest(result)
   }).then((secondResult) => {
-    console.log('PROMISE SECOND', secondResult)
+    console.log('request promise #2', secondResult)
 
     return remit
-      .request('my.worker.queue')
+      .request('my.worker.queue.2')
       .send({qux: 'quququ'})
   }).then((thirdResult) => {
-    console.log('PROMISE THIRD', thirdResult)
+    console.log('request promise #3', thirdResult)
   }).catch((err) => {
-    console.log('PROMISE ERR', err)
+    console.log('request promise err #1', err)
   })
