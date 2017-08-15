6c991212458133aa7847c8fe1727cebad1def8d4
Jack Williams <jpwilliamsphotography@gmail.com>
2017-08-15 14:13:10 +0100

    ✅ Add basic remit.listen tests

--- test/listener.test.js
+++ test/listener.test.js
@@ -9,12 +9,40 @@ describe('Listener', function () {
   })
 
   describe('#object', function () {
-    it('should be a function')
-    it('should expose "on" global function')
-    it('should return a Listener')
-    it('should expose a "handler" function')
-    it('should expose an "on" function')
-    it('should expose an "options" function')
-    it('should expose a "start" function')
+    it('should be a function', function () {
+      expect(remit.listen).to.be.a('function')
+    })
+
+    it('should expose "on" global function', function () {
+      expect(remit.listen.on).to.be.a('function')
+    })
+  })
+
+  describe('#return', function () {
+    let listener
+
+    before(async function () {
+      listener = remit.listen('foo')
+    })
+
+    it('should return a Listener', function () {
+      expect(listener).to.be.an.instanceof(remit.listen.Type)
+    })
+
+    it('should expose a "handler" function', function () {
+      expect(listener.handler).to.be.a('function')
+    })
+
+    it('should expose an "on" function', function () {
+      expect(listener.on).to.be.a('function')
+    })
+
+    it('should expose an "options" function', function () {
+      expect(listener.options).to.be.a('function')
+    })
+
+    it('should expose a "start" function', function () {
+      expect(listener.start).to.be.a('function')
+    })
   })
 })
