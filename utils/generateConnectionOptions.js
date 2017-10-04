f0c42b1289aa043c8ae9a819061f66d7f6a15c86
Jack Williams <jpwilliamsphotography@gmail.com>
2017-10-04 17:06:13 +0100

    💬 Add more client properties for help tracking rogue consumers
    Includes hostname, platform, PID and node versions

--- utils/generateConnectionOptions.js
+++ utils/generateConnectionOptions.js
@@ -1,3 +1,4 @@
+const os = require('os')
 const packageJson = require('../package.json')
 
 function generateConnectionOptions (name) {
@@ -6,7 +7,13 @@ function generateConnectionOptions (name) {
       connection_name: name,
       powered_by: `${packageJson.name}@${packageJson.version} (${packageJson.repository.url.substr(0, packageJson.repository.url.length - 4)}/tree/${packageJson.version})`,
       repository: packageJson.repository.url,
-      package: `https://www.npmjs.com/package/${packageJson.name}`
+      package: `https://www.npmjs.com/package/${packageJson.name}`,
+      host: {
+        name: os.hostname(),
+        platform: `${os.type()}@${os.release()}`,
+        pid: process.pid,
+        node: process.version
+      }
     }
   }
 }
