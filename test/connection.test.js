77ea1f4ce47ed06a409056fe0c3b750b217ee34e
Jack Tuck <jack-tuck@hotmail.com>
2018-01-27 20:58:11 +0000

    Added test for connection and awaiting throwAsException

---
+++ test/connection.test.js
@@ -0,0 +1,28 @@
+/* global describe, it, before, expect */
+describe('Connection', function () {
+  describe('#connect', function () {
+    it(`connection should throw`, done => {
+      const url = 'amqp://fuck'
+      const Remit = require('../')
+
+      var originalException = process.listeners('uncaughtException').pop()
+
+      process.removeListener('uncaughtException', originalException);
+      process.once("uncaughtException", function (error) {
+        recordedError = error
+        expect(recordedError.errno).to.equal('ENOTFOUND')
+        done()
+      })
+
+      const remit = Remit({ url })
+
+      remit
+        .request('foo')
+        .send({})
+
+      process.nextTick(function () {
+        process.listeners('uncaughtException').push(originalException)
+      })
+    })
+  })
+})
