9014e69f4bde062c280647de137fd2cc2b5adecb
Jack Williams <jpwilliamsphotography@gmail.com>
2019-12-22 12:02:00 +0000

    bug chunk for listener and endpoint closer, stub for Remit instance

---
+++ utils/states.js
@@ -0,0 +1,9 @@
+module.exports = {
+	STOPPED: 'stopped',
+	STOPPING: 'stopping',
+	CONSUMING: 'consuming',
+	SETTINGUP: 'settingup',
+	PAUSING: 'pausing',
+	PAUSED: 'paused',
+	RESUMING: 'resuming'
+}
